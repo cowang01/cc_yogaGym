42ce44b52b7c507e44eb4cfc173fc770174102c6
cowang01 <g_cowan27@hotmail.com>
2019-05-31 14:54:02 +0100

    shanti-db.sql

---
+++ db/shanti.sql
@@ -0,0 +1,55 @@
+DROP TABLE IF EXISTS feedbacks CASCADE;
+DROP TABLE IF EXISTS sessions CASCADE;
+DROP TABLE IF EXISTS members CASCADE;
+DROP TABLE IF EXISTS teachers CASCADE;
+DROP TABLE IF EXISTS rooms CASCADE;
+DROP TABLE IF EXISTS types CASCADE;
+
+
+
+
+CREATE TABLE types (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(64),
+  about VARCHAR(255)
+);
+
+CREATE TABLE rooms (
+  id SERIAL8 PRIMARY KEY,
+  title VARCHAR(64),
+  size INT4,
+  image VARCHAR(64)
+);
+
+CREATE TABLE teachers (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(64),
+  bio VARCHAR(255),
+  profile VARCHAR(64)
+);
+
+CREATE TABLE members (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(64),
+  join_date DATE,
+  waver BOOLEAN NOT NULL,
+  info VARCHAR(255),
+  membership DATE
+);
+
+CREATE TABLE sessions (
+  id SERIAL8 PRIMARY KEY,
+  event_date DATE,
+  event_time TIME,
+  room_id INT8 REFERENCES rooms(id),
+  teacher_id INT8 REFERENCES teachers(id),
+  member_id INT8 REFERENCES members(id),
+  type_id INT8 REFERENCES types(id),
+  status VARCHAR(64)
+);
+
+CREATE TABLE feedbacks (
+  id SERIAL8 PRIMARY KEY,
+  session_id INT8 REFERENCES sessions(id),
+  feedback VARCHAR(255)
+);
