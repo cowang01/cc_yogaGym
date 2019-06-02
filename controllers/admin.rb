e99d13e616348ffa5a9b6b7d406f2e0ab8fe11e7
cowang01 <g_cowan27@hotmail.com>
2019-06-02 12:45:02 +0100

    yoga_gym-admin menu

--- controllers/admin.rb
+++ controllers/admin.rb
@@ -8,3 +8,13 @@ require_relative('../models/teacher.rb')
 require_relative('../models/member.rb')
 require_relative('../models/session.rb')
 require_relative('../models/feedback.rb')
+
+
+get '/gym/admin' do
+  erb(:'admin/log_in')
+end
+
+get '/gym/admenu' do
+  @teacher = Teacher.find(params['id'].to_i)
+  erb(:'admin/menu')
+end
--- yoga_gym.rb
+++ yoga_gym.rb
@@ -1,9 +1,14 @@
 require( 'sinatra' )
 require( 'sinatra/contrib/all' )
+also_reload('./*')
 require('pry')
 
 require_relative('./controllers/admin.rb')
 
-get '/shanti' do
+get '/gym' do
   erb(:welcome)
+end
+
+get '/gym/log_in' do
+  erb(:log_in)
 end
